eeaadf243e61c66e25294bf4f2a327780f9a96e8
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-29 18:26:29 +0700

    controller execute to get time by the time city is switched

--- src/main/java/com/codegym/controller/AjaxController.java
+++ src/main/java/com/codegym/controller/AjaxController.java
@@ -3,9 +3,16 @@ package com.codegym.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Controller
 public class AjaxController {
@@ -15,6 +22,16 @@ public class AjaxController {
         LocalDateTime now = LocalDateTime.now();
         model.addAttribute("timenow", dtf.format(now));
         return "index";
+    }
+
+    @PostMapping(value = "/gettimezone")
+    @ResponseBody
+    public String getTimezone(@RequestParam("val") String timezone) {
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        df.setTimeZone(TimeZone.getTimeZone(timezone));
+        String curentdate = df.format(date);
+        return curentdate;
     }
 
 }
